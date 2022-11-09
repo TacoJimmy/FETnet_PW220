646bb9ea323e9b02052c72ba53117632ca3d6705
TacoJimmy <t5669016@ntut.org.tw>
2022-11-09 13:07:05 +0800

    Update FET_Flask.py

--- FET_Flask.py
+++ FET_Flask.py
@@ -317,7 +317,6 @@ def save_data(a, b):
     datatime = time.strftime("%Y-%m-%d-%H:%M:%S")
     with open('static/data/PowerSubLoop01.json', 'r') as a:
         subpower01 = json.load(a)
-        
         subpower01["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop01.csv", "a", newline="")as csvfile:
@@ -325,133 +324,107 @@ def save_data(a, b):
     csvfile.close
     
     with open('static/data/PowerSubLoop02.json', 'r') as a:
-        #subpower02 = json.load(a)
-        subpower02 = a
-        subpower02["datatime"] = str(datatime)
+        subpower02 = json.load(a)
+        csv.dump(subpower02, csvfile)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop02.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower02)
+        csv.dump(subpower02, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop03.json', 'r') as a:
-        #subpower03 = json.load(a)
-        subpower03 = a
+        subpower03 = json.load(a)
         subpower03["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop03.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower03)
+        csv.dump(subpower03, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop04.json', 'r') as a:
-        #subpower04 = json.load(a)
-        subpower04 = a
+        subpower04 = json.load(a)
         subpower04["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop04.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower04)
+        csv.dump(subpower04, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop05.json', 'r') as a:
-        #subpower05 = json.load(a)
-        subpower05 = a
+        subpower05 = json.load(a)
         subpower05["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop05.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower05)
+        csv.dump(subpower05, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop06.json', 'r') as a:
-        #subpower06 = json.load(a)
-        subpower06 = a
+        subpower06 = json.load(a)
         subpower06["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop06.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower06)
+        csv.dump(subpower06, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop07.json', 'r') as a:
-        #subpower07 = json.load(a)
-        subpower07 = a
+        subpower07 = json.load(a)
         subpower07["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop07.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower07)
+        csv.dump(subpower07, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop08.json', 'r') as a:
-        #subpower08 = json.load(a)
-        subpower08 = a
+        subpower08 = json.load(a)
         subpower08["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop08.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower08)
+        csv.dump(subpower08, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop09.json', 'r') as a:
-        #subpower09 = json.load(a)
-        subpower09 = a
+        subpower09 = json.load(a)
         subpower09["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop09.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower09)
+        csv.dump(subpower09, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop10.json', 'r') as a:
-        #subpower10 = json.load(a)
-        subpower10 = a
+        subpower10 = json.load(a)
         subpower10["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop10.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower10)
+        csv.dump(subpower10, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop11.json', 'r') as a:
-        #subpower11 = json.load(a)
-        subpower11 = a
+        subpower11 = json.load(a)
         subpower11["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop11.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower11)
+        csv.dump(subpower11, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop12.json', 'r') as a:
-        #subpower12 = json.load(a)
-        subpower12 = a
+        subpower12 = json.load(a)
         subpower12["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop12.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower12)
+        csv.dump(subpower12, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop13.json', 'r') as a:
-        #subpower13 = json.load(a)
-        subpower13 = a
+        subpower13 = json.load(a)
         subpower13["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop13.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower13)
+        csv.dump(subpower13, csvfile)
     csvfile.close
 
     with open('static/data/PowerSubLoop14.json', 'r') as a:
-        #subpower14 = json.load(a)
-        subpower14 = a
+        subpower14 = json.load(a)
         subpower14["datatime"] = str(datatime)
     a.close
     with open("/media/mmcblk0p1/"+time.strftime("%Y-%m-%d")+"-SubLoop14.csv", "a", newline="")as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(subpower14)
+        csv.dump(subpower14, csvfile)
     csvfile.close
 
 
